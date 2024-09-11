7b371a7ae0f4debdaec42a2bee060bee95a32405
yura-prots <yuraprots@gmail.com>
2024-09-11 11:43:55 +0300

    before expo update

--- crash-course/app/index.tsx
+++ crash-course/app/index.tsx
@@ -5,7 +5,7 @@ import { Text, View } from "react-native";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">App!</Text>
+      <Text className="text-3xl font-pblack">App!</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{ color: "blue" }}>
         Go To Profile
