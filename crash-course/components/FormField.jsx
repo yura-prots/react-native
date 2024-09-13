b0139723b8f5888c6b6b1d6d982dff41dbc4aca6
yura-prots <yuraprots@gmail.com>
2024-09-13 21:33:03 +0300

    created FormField

---
+++ crash-course/components/FormField.jsx
@@ -0,0 +1,23 @@
+import { View, Text, TextInput } from "react-native";
+import React from "react";
+
+const FormField = ({
+  title,
+  value,
+  placeholder,
+  handleChangeText,
+  otherStyles,
+  ...props
+}) => {
+  return (
+    <View className={`space-y-2 ${otherStyles}`}>
+      <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
+
+      <View className="w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center border-2 border-black-200">
+        <TextInput />
+      </View>
+    </View>
+  );
+};
+
+export default FormField;
