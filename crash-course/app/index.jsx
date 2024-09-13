09ec0d772499f89847ec1868289bbd2abe8b89c8
yura-prots <yuraprots@gmail.com>
2024-09-13 18:18:38 +0300

    created CustomButton

--- crash-course/app/index.jsx
+++ crash-course/app/index.jsx
@@ -1,6 +1,10 @@
-import { images } from "@/constants";
 import { Image, ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import { router } from "expo-router";
+
+import { images } from "@/constants";
+import CustomButton from "../components/CustomButton";
 
 export default function App() {
   return (
@@ -23,9 +27,22 @@ export default function App() {
             <Text className="text-3xl text-white font-bold text-center">
               Discover with <Text className="text-secondary-200">Aora</Text>
             </Text>
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              resizeMode="contain"
+            />
           </View>
+
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push("/sign-in")}
+            containerStyles="w-full mt-7"
+          />
         </View>
       </ScrollView>
+
+      <StatusBar backgroundColor="#161622" style="light" />
     </SafeAreaView>
   );
 }
