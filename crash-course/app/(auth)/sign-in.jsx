66536dfacda12d15c0f4843bbf7cdd5f61095262
yura-prots <yuraprots@gmail.com>
2024-09-14 10:12:27 +0300

    create FormField

--- crash-course/app/(auth)/sign-in.jsx
+++ crash-course/app/(auth)/sign-in.jsx
@@ -4,17 +4,22 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 import { images } from "@/constants";
 import FormField from "@/components/FormField";
+import CustomButton from "@/components/CustomButton";
+import { Link } from "expo-router";
 
 const SignIn = () => {
   const [form, setForm] = useState({
     email: "",
     password: "",
   });
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const submit = () => {};
 
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
-        <View className="w-full justify-center h-full px-4 my-6">
+        <View className="w-full justify-center min-h-[85vh] px-4 my-6">
           <Image
             source={images.logo}
             resizeMode="contain"
@@ -38,6 +43,25 @@ const SignIn = () => {
             handleChangeText={e => setForm({ ...form, password: e })}
             otherStyles="mt-7"
           />
+
+          <CustomButton
+            title="Sigh In"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Don't have account?
+            </Text>
+
+            <Link
+              href="/(auth)/sign-up"
+              className="text-lg font-psemibold text-secondary">
+              Sign Up
+            </Link>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
